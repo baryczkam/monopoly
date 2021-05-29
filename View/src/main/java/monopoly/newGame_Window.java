643aa967555d5a27e23ef92c3697d39bd0641062
jdl123 <julia.dlubak00@gmail.com>
2021-05-29 14:09:17 +0200

    rozpoczęcie pracy nad tworzeniem graczy i wyborem pionków

--- View/src/main/java/monopoly/newGame_Window.java
+++ View/src/main/java/monopoly/newGame_Window.java
@@ -2,25 +2,43 @@ package monopoly;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.TextField;
 
-
-import java.awt.*;
-//import javafx.scene.control.ChoiceBox;
-//import javafx.scene.control.monopoly.ComboBox;
-//import javafx.scene.control.TextField;
+import static javafx.collections.FXCollections.observableArrayList;
 
 public class newGame_Window implements Mediator {
 
     @FXML
-//    private ChoiceBox chooseNumberOfPlayers;
-    private ComboBox chooseNumberOfPlayers;
+    private ComboBox<String> chooseNumberOfPlayers;
 
     @FXML
-    private TextBox writeName;
+    private ComboBox<String> choosePawn1;
+    @FXML
+    private ComboBox<String> choosePawn2;
+    @FXML
+    private ComboBox<String> choosePawn3;
+    @FXML
+    private ComboBox<String> choosePawn4;
 
     @FXML
-//    private ChoiceBox choosePawn;
-    private ComboBox choosePawn;
+    public void initialize() {
+        chooseNumberOfPlayers.setItems(observableArrayList("1","2","3","4"));
+        choosePawn1.setItems(observableArrayList("Auto","But","Kapelusz","Pies","Statek","Taczka"));
+        choosePawn2.setItems(observableArrayList("Auto","But","Kapelusz","Pies","Statek","Taczka"));
+        choosePawn3.setItems(observableArrayList("Auto","But","Kapelusz","Pies","Statek","Taczka"));
+        choosePawn4.setItems(observableArrayList("Auto","But","Kapelusz","Pies","Statek","Taczka"));
+    }
+
+    @FXML
+    private TextField writeName1;
+    @FXML
+    private TextField writeName2;
+    @FXML
+    private TextField writeName3;
+    @FXML
+    private TextField writeName4;
+
 
     @FXML
     private Button startNewGame;
@@ -36,5 +54,9 @@ public class newGame_Window implements Mediator {
     @FXML
     public void returnToMainWindow() {
         BuildingStage.buildStage("/Main_Window.fxml");
+    }
+
+    public void createPlayer() {
+
     }
 }
